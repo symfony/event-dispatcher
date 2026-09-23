symfony/symfony

[DependencyInjection][EventDispatcher] Order tagged services and listeners with before/after constraints
Priorities cannot express "run between these two" when the two services are
declared by bundles you do not control and share a priority. This adds "before"
and "after" constraints, resolved at compile time into an order.

Constraints are applied on top of the existing order: the priority sort seeds the
result, then items move only as far as the constraints require. A target that is
not installed is ignored, so an optional dependency does not become fatal, and a
cycle throws naming the services it runs through.

Event listeners are covered too. Their runtime order is (priority DESC, insertion
ASC), so the pass reorders the addListener() calls and raises a priority only when
a constraint puts a listener ahead of a higher-priority one. A collection with no
constraint keeps byte-identical calls.

src/Symfony/Component/EventDispatcher/DependencyInjection/RegisterListenersPass.php
@@ -12,8 +12,10 @@
 namespace Symfony\Component\EventDispatcher\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\Argument\ServiceClosureArgument;
+use Symfony\Component\DependencyInjection\Compiler\BeforeAfterSorter;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\EventDispatcher\EventDispatcher;
@@ -80,6 +82,7 @@ class RegisterListenersPass implements CompilerPassInterface
         }
 
         $globalDispatcherDefinition = $container->findDefinition('event_dispatcher');
+        $calls = [];
 
         foreach ($container->findTaggedServiceIds('kernel.event_listener', true) as $id => $events) {
             $noPreload = 0;
@@ -127,7 +130,14 @@ class RegisterListenersPass implements CompilerPassInterface
                     $dispatcherDefinition = $container->findDefinition($event['dispatcher']);
                 }
 
-                $dispatcherDefinition->addMethodCall('addListener', [$event['event'], [new ServiceClosureArgument(new Reference($id)), $event['method']], $priority]);
+                $constraints = [];
+                foreach (['before', 'after'] as $direction) {
+                    if ($targets = (array) ($event[$direction] ?? [])) {
+                        $constraints[$direction] = $targets;
+                    }
+                }
+
+                $calls[] = [$dispatcherDefinition, $id, [$event['event'], [new ServiceClosureArgument(new Reference($id)), $event['method']], $priority], $constraints];
 
                 if (isset($hotPathEvents[$event['event']])) {
                     $container->getDefinition($id)->addTag('container.hot_path');
@@ -177,7 +187,7 @@ class RegisterListenersPass implements CompilerPassInterface
             foreach ($extractingDispatcher->listeners as $args) {
                 $args[1] = [new ServiceClosureArgument(new Reference($id)), $args[1]];
                 foreach ($dispatcherDefinitions as $dispatcherDefinition) {
-                    $dispatcherDefinition->addMethodCall('addListener', $args);
+                    $calls[] = [$dispatcherDefinition, $id, $args, []];
                 }
 
                 if (isset($hotPathEvents[$args[0]])) {
@@ -192,6 +202,148 @@ class RegisterListenersPass implements CompilerPassInterface
             $extractingDispatcher->listeners = [];
             ExtractingEventDispatcher::$aliases = [];
         }
+
+        $this->addListenerCalls($container, $calls);
+    }
+
+    /**
+     * @param list<array{0: Definition, 1: string, 2: array, 3: array}> $calls Dispatcher definition, service id, addListener() arguments and "before"/"after" constraints
+     */
+    private function addListenerCalls(ContainerBuilder $container, array $calls): void
+    {
+        $groups = [];
+
+        foreach ($calls as $i => [$dispatcherDefinition, , $args]) {
+            $groups[spl_object_id($dispatcherDefinition)."\0".$args[0]][] = $i;
+        }
+
+        foreach ($groups as $group) {
+            foreach ($this->sortByConstraints($container, $calls, $group) as $i => $call) {
+                $calls[$group[$i]] = $call;
+            }
+        }
+
+        foreach ($calls as [$dispatcherDefinition, , $args]) {
+            $dispatcherDefinition->addMethodCall('addListener', $args);
+        }
+    }
+
+    /**
+     * Rejects a "Service::method" target naming a method the service does not listen to.
+     *
+     * A target whose service part is absent stays ignored: the package declaring it may not be installed.
+     * One that is present has to name a real listener, otherwise the constraint would silently do nothing.
+     *
+     * @param array<string, array{before?: list<string>, after?: list<string>}> $constraints
+     * @param array<string, list<string>>                                       $aliases
+     */
+    private function checkTargetMethods(array $constraints, array $aliases, string $event): void
+    {
+        foreach ($constraints as $key => $constraint) {
+            foreach ($constraint as $direction => $targets) {
+                foreach ($targets as $target) {
+                    if (isset($aliases[$target]) || false === $i = strrpos($target, '::')) {
+                        continue;
+                    }
+
+                    $service = substr($target, 0, $i);
+
+                    if (isset($aliases[$service])) {
+                        throw new InvalidArgumentException(\sprintf('Invalid "%s" constraint on listener "%s": "%s" does not listen to event "%s" with method "%s".', $direction, $key, $service, $event, substr($target, 2 + $i)));
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Reorders the calls listening to one event on one dispatcher so that their "before"/"after" constraints are met.
+     *
+     * The runtime order is (priority DESC, insertion ASC), so the constraints are turned into an insertion
+     * order, and priorities are raised whenever a constraint puts a listener ahead of a higher-priority one.
+     *
+     * @param list<array{0: Definition, 1: string, 2: array, 3: array}> $calls
+     * @param list<int>                                                 $group The indexes of the calls to reorder
+     *
+     * @return list<array{0: Definition, 1: string, 2: array, 3: array}> The reordered calls, or none when the group has no constraint to apply
+     */
+    private function sortByConstraints(ContainerBuilder $container, array $calls, array $group): array
+    {
+        $constrained = false;
+
+        foreach ($group as $i) {
+            if ($calls[$i][3]) {
+                $constrained = true;
+                break;
+            }
+        }
+
+        if (!$constrained) {
+            return [];
+        }
+
+        $seed = $group;
+        usort($seed, static fn ($a, $b) => $calls[$b][2][2] <=> $calls[$a][2][2] ?: $a <=> $b);
+
+        $keys = [];
+        $indexes = [];
+        $aliases = [];
+        $constraints = [];
+
+        foreach ($seed as $i) {
+            $id = $calls[$i][1];
+
+            for ($n = 0, $key = $id; isset($indexes[$key]); ++$n) {
+                $key = $id.'#'.$n;
+            }
+
+            $method = $calls[$i][2][1][1];
+            $aliases[$id][] = $key;
+            $aliases[$id.'::'.$method][] = $key;
+            $indexes[$key] = $i;
+            $keys[] = $key;
+
+            if ($calls[$i][3]) {
+                $constraints[$key] = $calls[$i][3];
+            }
+
+            if ($class = $container->getParameterBag()->resolveValue($container->getDefinition($id)->getClass())) {
+                $aliases[$class][] = $key;
+                $aliases[$class.'::'.$method][] = $key;
+            }
+        }
+
+        // a service id always designates its own registrations, whatever class it shares a name with
+        foreach ($seed as $i) {
+            $id = $calls[$i][1];
+            $aliases[$id] = array_values(array_unique($aliases[$id]));
+            $aliases[$id.'::'.$calls[$i][2][1][1]] = array_values(array_unique($aliases[$id.'::'.$calls[$i][2][1][1]]));
+        }
+
+        $this->checkTargetMethods($constraints, $aliases, $calls[$group[0]][2][0]);
+
+        try {
+            $sortedKeys = BeforeAfterSorter::sort($keys, $constraints, $aliases);
+        } catch (InvalidArgumentException $e) {
+            throw new InvalidArgumentException(\sprintf('Invalid "before"/"after" constraints for event "%s": ', $calls[$group[0]][2][0]).lcfirst($e->getMessage()), previous: $e);
+        }
+
+        if ($sortedKeys === $keys) {
+            // the constraints changed nothing, leave the calls exactly as they were emitted
+            return [];
+        }
+
+        $sorted = [];
+        foreach ($sortedKeys as $key) {
+            $sorted[] = $calls[$indexes[$key]];
+        }
+
+        $floor = \PHP_INT_MIN;
+        for ($i = \count($sorted) - 1; 0 <= $i; --$i) {
+            $sorted[$i][2][2] = $floor = max($sorted[$i][2][2], $floor);
+        }
+
+        return $sorted;
     }
 
     /**

symfony/event-dispatcher

DependencyInjection/RegisterListenersPass.php
@@ -12,8 +12,10 @@
 namespace Symfony\Component\EventDispatcher\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\Argument\ServiceClosureArgument;
+use Symfony\Component\DependencyInjection\Compiler\BeforeAfterSorter;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\EventDispatcher\EventDispatcher;
@@ -80,6 +82,7 @@ class RegisterListenersPass implements CompilerPassInterface
         }
 
         $globalDispatcherDefinition = $container->findDefinition('event_dispatcher');
+        $calls = [];
 
         foreach ($container->findTaggedServiceIds('kernel.event_listener', true) as $id => $events) {
             $noPreload = 0;
@@ -127,7 +130,14 @@ class RegisterListenersPass implements CompilerPassInterface
                     $dispatcherDefinition = $container->findDefinition($event['dispatcher']);
                 }
 
-                $dispatcherDefinition->addMethodCall('addListener', [$event['event'], [new ServiceClosureArgument(new Reference($id)), $event['method']], $priority]);
+                $constraints = [];
+                foreach (['before', 'after'] as $direction) {
+                    if ($targets = (array) ($event[$direction] ?? [])) {
+                        $constraints[$direction] = $targets;
+                    }
+                }
+
+                $calls[] = [$dispatcherDefinition, $id, [$event['event'], [new ServiceClosureArgument(new Reference($id)), $event['method']], $priority], $constraints, $event['priority'] ?? null];
 
                 if (isset($hotPathEvents[$event['event']])) {
                     $container->getDefinition($id)->addTag('container.hot_path');
@@ -174,10 +184,10 @@ class RegisterListenersPass implements CompilerPassInterface
             ExtractingEventDispatcher::$aliases = $aliases;
             ExtractingEventDispatcher::$subscriber = $class;
             $extractingDispatcher->addSubscriber($extractingDispatcher);
-            foreach ($extractingDispatcher->listeners as $args) {
-                $args[1] = [new ServiceClosureArgument(new Reference($id)), $args[1]];
+            foreach ($extractingDispatcher->listeners as [$eventName, $method, $priority, $constraints]) {
+                $args = [$eventName, [new ServiceClosureArgument(new Reference($id)), $method], $priority ?? 0];
                 foreach ($dispatcherDefinitions as $dispatcherDefinition) {
-                    $dispatcherDefinition->addMethodCall('addListener', $args);
+                    $calls[] = [$dispatcherDefinition, $id, $args, $constraints, $priority];
                 }
 
                 if (isset($hotPathEvents[$args[0]])) {
@@ -192,6 +202,146 @@ class RegisterListenersPass implements CompilerPassInterface
             $extractingDispatcher->listeners = [];
             ExtractingEventDispatcher::$aliases = [];
         }
+
+        $this->addListenerCalls($container, $calls);
+    }
+
+    /**
+     * @param list<array{0: Definition, 1: string, 2: array, 3: array, 4: int|null}> $calls Dispatcher definition, service id, addListener() arguments, "before"/"after" constraints and declared priority
+     */
+    private function addListenerCalls(ContainerBuilder $container, array $calls): void
+    {
+        $groups = [];
+
+        foreach ($calls as $i => [$dispatcherDefinition, , [$event]]) {
+            $groups[spl_object_id($dispatcherDefinition)."\0".$event][] = $i;
+        }
+
+        foreach ($groups as $group) {
+            foreach ($this->sortByConstraints($container, $calls, $group) as $i => $call) {
+                $calls[$group[$i]] = $call;
+            }
+        }
+
+        foreach ($calls as [$dispatcherDefinition, , $args]) {
+            $dispatcherDefinition->addMethodCall('addListener', $args);
+        }
+    }
+
+    /**
+     * Rejects a "Service::method" target naming a method the service does not listen to.
+     *
+     * A target whose service part is absent stays ignored: the package declaring it may not be installed.
+     * One that is present has to name a real listener, otherwise the constraint would silently do nothing.
+     *
+     * @param array<string, array{before?: list<string>, after?: list<string>}> $constraints
+     * @param array<string, list<string>>                                       $aliases
+     */
+    private function checkTargetMethods(array $constraints, array $aliases, string $event): void
+    {
+        foreach ($constraints as $key => $constraint) {
+            foreach ($constraint as $direction => $targets) {
+                foreach ($targets as $target) {
+                    if (isset($aliases[$target]) || false === $i = strrpos($target, '::')) {
+                        continue;
+                    }
+
+                    $service = substr($target, 0, $i);
+
+                    if (isset($aliases[$service])) {
+                        throw new InvalidArgumentException(\sprintf('Invalid "%s" constraint on listener "%s": "%s" does not listen to event "%s" with method "%s".', $direction, $key, $service, $event, substr($target, 2 + $i)));
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Reorders the calls listening to one event on one dispatcher so that their "before"/"after" constraints are met.
+     *
+     * The runtime order is (priority DESC, insertion ASC), so the constraints are turned into an insertion
+     * order. A declared priority is never changed; a listener without one takes the priority its place needs.
+     *
+     * @param list<array{0: Definition, 1: string, 2: array, 3: array, 4: int|null}> $calls
+     * @param list<int>                                                              $group The indexes of the calls to reorder
+     *
+     * @return list<array{0: Definition, 1: string, 2: array, 3: array, 4: int|null}> The reordered calls, or none when the group has no constraint to apply
+     */
+    private function sortByConstraints(ContainerBuilder $container, array $calls, array $group): array
+    {
+        $constrained = false;
+
+        foreach ($group as $i) {
+            [, , , $callConstraints] = $calls[$i];
+
+            if ($callConstraints) {
+                $constrained = true;
+                break;
+            }
+        }
+
+        if (!$constrained) {
+            return [];
+        }
+
+        [, , [$event]] = $calls[$group[0]];
+        $seed = $group;
+        usort($seed, static fn ($a, $b) => $calls[$b][2][2] <=> $calls[$a][2][2] ?: $a <=> $b);
+
+        $keys = [];
+        $indexes = [];
+        $priorities = [];
+        $aliases = [];
+        $keysById = [];
+        $constraints = [];
+
+        foreach ($seed as $i) {
+            [, $id, [, [, $method]], $callConstraints, $declaredPriority] = $calls[$i];
+
+            // keys show up in error messages: the service id, then "id::method", then a counter for a repeated method
+            for ($n = 0, $key = $id; isset($indexes[$key]); ++$n) {
+                $key = $id.'::'.$method.($n ? '#'.$n : '');
+            }
+
+            $keysById[$id][] = $key;
+            $keysById[$id.'::'.$method][] = $key;
+            $indexes[$key] = $i;
+            $keys[] = $key;
+            $priorities[$key] = $declaredPriority;
+
+            if ($callConstraints) {
+                $constraints[$key] = $callConstraints;
+            }
+
+            if ($class = $container->getParameterBag()->resolveValue($container->getDefinition($id)->getClass())) {
+                $aliases[$class][] = $key;
+                $aliases[$class.'::'.$method][] = $key;
+            }
+        }
+
+        // a service id always designates its own registrations, whatever class it shares a name with
+        $aliases = $keysById + $aliases;
+
+        $this->checkTargetMethods($constraints, $aliases, $event);
+
+        try {
+            $resolved = BeforeAfterSorter::sortWithPriorities($priorities, $constraints, $aliases);
+        } catch (InvalidArgumentException $e) {
+            throw new InvalidArgumentException(\sprintf('Invalid "before"/"after" constraints for event "%s": ', $event).lcfirst($e->getMessage()), previous: $e);
+        }
+
+        $sorted = [];
+        $untouched = true;
+        $n = 0;
+
+        foreach ($resolved as $key => $priority) {
+            [$dispatcherDefinition, $id, [$event, $listener, $emittedPriority], $callConstraints, $declaredPriority] = $calls[$indexes[$key]];
+            $untouched = $untouched && $key === $keys[$n++] && $priority === $emittedPriority;
+            $sorted[] = [$dispatcherDefinition, $id, [$event, $listener, $priority], $callConstraints, $declaredPriority];
+        }
+
+        // when nothing moved and no priority changed, leave the calls exactly as they were emitted
+        return $untouched ? [] : $sorted;
     }
 
     /**
@@ -244,6 +394,38 @@ class ExtractingEventDispatcher extends EventDispatcher implements EventSubscrib
     public function addListener(string $eventName, callable|array $listener, int $priority = 0): void
     {
         $this->listeners[] = [$eventName, $listener[1], $priority];
+    }
+
+    /**
+     * Same as the parent, but records whether the subscriber declared a priority, and its "before"/"after" constraints.
+     */
+    public function addSubscriber(EventSubscriberInterface $subscriber): void
+    {
+        foreach ($subscriber->getSubscribedEvents() as $eventName => $params) {
+            if (\is_string($params)) {
+                $this->listeners[] = [$eventName, $params, null, []];
+            } elseif (isset($params['method'])) {
+                $this->listeners[] = [$eventName, $params['method'], $params['priority'] ?? null, self::getConstraints($params)];
+            } elseif (\is_string($params[0])) {
+                $this->listeners[] = [$eventName, $params[0], $params[1] ?? null, []];
+            } else {
+                foreach ($params as $listener) {
+                    $this->listeners[] = [$eventName, $listener['method'] ?? $listener[0], $listener['priority'] ?? $listener[1] ?? null, self::getConstraints($listener)];
+                }
+            }
+        }
+    }
+
+    private static function getConstraints(array $params): array
+    {
+        $constraints = [];
+        foreach (['before', 'after'] as $direction) {
+            if ($targets = (array) ($params[$direction] ?? [])) {
+                $constraints[$direction] = $targets;
+            }
+        }
+
+        return $constraints;
     }
 
     public static function getSubscribedEvents(): array